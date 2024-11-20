0a0283035b537718bce79c2738da79ef9ce85fe4
MrDesilvestri <carlosdesilvestrir@javeriana.edu.co>
2024-11-19 19:01:38 -0500

    Fix: fixing cross-origin problem.

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
 from database import create_tables
 from routers import accommodations, access
 
 app = FastAPI()
+
+origins = [
+    "https://front-unlock-patrones.vercel.app",  # Dominio de tu frontend
+    "http://localhost:4200",  # Si estás probando en Angular localmente
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Permitir estos orígenes
+    allow_credentials=True,
+    allow_methods=["*"],  # Permitir todos los métodos HTTP
+    allow_headers=["*"],  # Permitir todos los encabezados
+)
 
 # Crear tablas en la base de datos en el inicio de la aplicación
 @app.on_event("startup")
